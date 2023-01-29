8d2b0b3a3526f404d5adf51ca5675b3ffa065b51
dbianchii <gdbianchii@gmail.com>
2023-01-28 23:57:21 -0300

    last commit ✅

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -10,14 +10,13 @@ export default function Cardapio() {
 	const [busca, setBusca] = useState("")
 	const [filtro, setFiltro] = useState<number | null>(null)
 	const [ordenador, setOrdenador] = useState("")
-
 	return (
 		<main>
 			<nav className={styles.menu}>
 				<Logo />
 			</nav>
 			<header className={styles.header}>
-				<div className={styles.header__text}></div>
+				<div className={styles.header__text}>A casa do código e da massa</div>
 			</header>
 			<section className={styles.cardapio}>
 				<h3 className={styles.cardapio__titulo}>Cardápio</h3>
